00e9e841de93fffa72889109c739aa67862bc7b8
KumarKuldipDas27 <kumarkuldipdas@gmail.com>
2024-12-02 22:24:30 +0530

    Add files via upload

---
+++ V2__Add_productds.sql
@@ -0,0 +1,12 @@
+INSERT INTO test_migration.Product (Product_Name, Price) 
+VALUES 
+    ('Laptop', 799.99),
+    ('Smartphone', 499.50),
+    ('Wireless Mouse', 25.99),
+    ('Keyboard', 45.00),
+    ('Monitor', 150.75),
+    ('External Hard Drive', 89.99),
+    ('Gaming Console', 299.99),
+    ('Headphones', 59.99),
+    ('Smartwatch', 199.00),
+    ('Webcam', 39.99);
