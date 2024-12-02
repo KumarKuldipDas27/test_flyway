4835879e24852a3ea1c9ef540a7f51b335d2d98b
Kumar Kuldip Das <kumarkuldipdas@gmail.com>
2024-12-03 01:30:12 +0530

    renamed the create product table

--- V1_Create_Product_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Product (
-    Product_ID SERIAL PRIMARY KEY,
-    Product_Name VARCHAR(255) NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL
-);
---
+++ V1__Create_Product_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Product (
+    Product_ID SERIAL PRIMARY KEY,
+    Product_Name VARCHAR(255) NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL
+);
